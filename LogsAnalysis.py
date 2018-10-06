259540da88c176ed6a7c0ca1699ce6fe9860021c
Sara-Mohamed-Ahmed-Genady <saragnady92@gmail.com>
2018-10-06 21:55:19 +0200

    Get Top authors

--- LogsAnalysis.py
+++ LogsAnalysis.py
@@ -19,6 +19,20 @@ print("What are the most popular three articles of all time?")
 for title, views_num in top_three_articles:
      print(" \"{}\" -- {} views".format(title, views_num))
 
+# Who are the most popular article authors of all time?
+top_authors =c.execute("""select authors.name, count(*) as views_num
+              from articles, authors, logs
+              where logs.status='200 OK'
+              and authors.id = articles.author
+              and articles.slug = substr(logs.path, 10)
+              group by authors.name
+              order by views_num desc""")
+# Print question
+print("Who are the most popular article authors of all time?")
+# Loop on authors and print
+for title, views_num in top_authors:
+     print(" \"{}\" -- {} views".format(title, views_num))
+
 # conn commit #
 conn.commit()
 
